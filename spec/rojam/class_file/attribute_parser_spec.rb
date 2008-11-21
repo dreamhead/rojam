3f48ce74f5be05df388ec3f97d4bf423646cbd95
Ye Zheng <dreamhead.cn@gmail.com>
2008-11-21 09:18:38 +0000

    Used new method for attribute and instruction sepc.

--- spec/rojam/class_file/attribute_parser_spec.rb
+++ spec/rojam/class_file/attribute_parser_spec.rb
@@ -2,66 +2,61 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/constant_pool_stub')
 
 describe Rojam::AttributeParser do
-  AttributeClass = Struct.new(:attribute_name_index, :infoes)
-  
   before(:each) do
     @pool = ConstantPoolStub.new
     @parser = Rojam::AttributeParser.new(@pool)
   end
-  
+
   it 'parses attributes' do
-    node = Rojam::MethodNode.new
-    attr = AttributeClass.new(0x06, [0x00, 0x01, 0x00, 0x01])
-    attributes = [attr, attr]
+    attributes = ['attr', 'attr']
     @parser.should_receive(:parse).twice
-    @parser.parse_attributes(attributes, node)
+    @node = Rojam::MethodNode.new
+    @parser.parse_attributes(attributes, @node)
   end
-  
+
   it 'raises exception for unknown attribute' do
-    node = Rojam::ClassNode.new
-    source_file_index = 0x06
-    attr = AttributeClass.new(source_file_index, [0x00, 0x09])
-    @pool.constant(source_file_index, "Unknown")
-    lambda { @parser.parse(attr, node) }.should raise_error
+    @node = Rojam::MethodNode.new
+    lambda { @parser.parse_attribute('Unknown', [0x00], @node) }.should raise_error
   end
-  
-  it 'parses SourceFile attribute' do
-    node = Rojam::ClassNode.new
-    source_file_index = 0x06
-    attr = AttributeClass.new(source_file_index, [0x00, 0x09])
-    @pool.constants({source_file_index => "SourceFile", 0x09 => "Blank.java"})
-    @parser.parse(attr, node)
-    node.source_file.should == 'Blank.java'
+
+  describe 'Method Attribute' do
+    before(:each) do
+      @node = Rojam::MethodNode.new
+    end
+    
+    it 'parses LineNumberTable' do
+      @parser.parse_attribute('LineNumberTable', [0x00, 0x01, 0x00, 0x00, 0x00, 0x01], @node)
+      @node.start_pc.should == 0x00
+      @node.line_number.should == 0x01
+    end
+
+    it "parses Code attribute" do
+      @pool.constants({
+          1 => Struct.new(:class_index, :name_and_type_index).new(0x06, 0x03),
+          2 => "java/lang/Object",
+          3 => Struct.new(:name_index, :descriptor_index).new(0x04, 0x05),
+          4 => "<init>",
+          5 => "()V",
+          6 => Struct.new(:name_index).new(0x02)
+        })
+      @parser.parse_attribute('Code',
+        [0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x05, 0x2A, 0xB7,
+          0x00, 0x01, 0xB1, 0x00, 0x00, 0x00, 0x00],
+        @node)
+      @node.max_stack.should == 0x01
+      @node.max_locals.should == 0x01
+    end
   end
-  
-  it 'parses LineNumberTable' do
-    node = Rojam::MethodNode.new
-    line_number_table_index = 0x07
-    attr = AttributeClass.new(line_number_table_index, [0x00, 0x01, 0x00, 0x00, 0x00, 0x01])
-    @pool.constant(line_number_table_index, "LineNumberTable")
-    @parser.parse(attr, node)
-    node.start_pc.should == 0x00
-    node.line_number.should == 0x01
-  end
-  
-  it "parses Code attribute" do
-    node = Rojam::MethodNode.new
-    code_index = 0x07
-    attr = AttributeClass.new(code_index,
-      [0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x05, 
-       0x2A, 0xB7, 0x00, 0x01, 0xB1, 0x00, 0x00, 0x00,
-       0x00])
-    @pool.constants({
-        1 => Struct.new(:class_index, :name_and_type_index).new(0x06, 0x03),
-        2 => "java/lang/Object",
-        3 => Struct.new(:name_index, :descriptor_index).new(0x04, 0x05),
-        4 => "<init>",
-        5 => "()V",
-        6 => Struct.new(:name_index).new(0x02),
-        7 => "Code"
-    })
-    @parser.parse(attr, node)
-    node.max_stack.should == 0x01
-    node.max_locals.should == 0x01
+
+  describe 'Class Attribute' do
+    before(:each) do
+      @node = Rojam::ClassNode.new
+    end
+
+    it 'parses SourceFile attribute' do
+      @pool.constants({0x09 => "Blank.java"})
+      @parser.parse_attribute('SourceFile', [0x00, 0x09], @node)
+      @node.source_file.should == 'Blank.java'
+    end
   end
 end
--- spec/rojam/class_file/instruction_parser_spec.rb
+++ spec/rojam/class_file/instruction_parser_spec.rb
@@ -5,20 +5,19 @@ describe Rojam::InstructionParser do
   before(:each) do
     @pool = ConstantPoolStub.new
     @parser = Rojam::InstructionParser.new @pool
-    @node = Rojam::MethodNode.new
   end
 
   it "parses ALOAD_0" do
-    @parser.parse(@node, [0x2A])
-    @node.instructions.should have(1).items
-    @node.instructions[0].opcode.should == Rojam::Opcode::ALOAD_0
+    instruction, consumed_byte_size = @parser.parse_instruction([0x2A])
+    instruction.opcode.should == Rojam::Opcode::ALOAD_0
   end
   
   it "parses ALOAD_0 sequentially" do
-    @parser.parse(@node, [0x2A, 0x2A])
-    @node.instructions.should have(2).items
-    @node.instructions[0].opcode.should == Rojam::Opcode::ALOAD_0
-    @node.instructions[1].opcode.should == Rojam::Opcode::ALOAD_0
+    node = Rojam::MethodNode.new
+    @parser.parse(node, [0x2A, 0x2A])
+    node.instructions.should have(2).items
+    node.instructions[0].opcode.should == Rojam::Opcode::ALOAD_0
+    node.instructions[1].opcode.should == Rojam::Opcode::ALOAD_0
   end
   
   it 'parses INVOKESPECIAL' do
@@ -30,12 +29,12 @@ describe Rojam::InstructionParser do
         5 => "()V",
         6 => Struct.new(:name_index).new(0x02)
     })
-    
-    @parser.parse(@node, [0xB7, 0x00, 0x01])
-    @node.instructions.should have(1).items
-    @node.instructions[0].opcode.should == Rojam::Opcode::INVOKESPECIAL
-    @node.instructions[0].owner.should == "java/lang/Object"
-    @node.instructions[0].name.should == "<init>"
-    @node.instructions[0].desc.should == "()V"
+
+    instruction, consumed_byte_size = @parser.parse_instruction([0xB7, 0x00, 0x01])
+    instruction.opcode.should == Rojam::Opcode::INVOKESPECIAL
+    consumed_byte_size.should == 3
+    instruction.owner.should == "java/lang/Object"
+    instruction.name.should == "<init>"
+    instruction.desc.should == "()V"
   end
 end
