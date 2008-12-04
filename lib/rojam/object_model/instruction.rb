d2203d8918bd5c7d7f97315c6a4a3b8f40867ef1
Ye Zheng <dreamhead.cn@gmail.com>
2008-12-04 11:57:40 +0000

    simplified different kinds of instructions definition.

--- lib/rojam/object_model/instruction.rb
+++ lib/rojam/object_model/instruction.rb
@@ -10,4 +10,30 @@ module Rojam
       (self.class == insn.class) && (@opcode == insn.opcode)
     end
   end
+
+  {
+    :Method => [:owner, :name, :desc],
+    :Field  => [:owner, :name, :desc],
+    :Var    => [:var],
+    :Type   => [:type],
+    :Int    => [:operand],
+    :Iinc   => [:var, :incr],
+    :Jump   => [:label],
+    :Ldc    => [:constant],
+  }.each do |type, args|
+    module_eval <<-INSTRUCDTION
+    class #{type}Insn < Instruction
+      attr_accessor #{args.collect{|item| ":#{item}"}.join(',')}
+
+      def initialize(opcode, #{args.collect{|item| "#{item}"}.join(',')})
+        super(opcode)
+        #{args.collect{|item| "@#{item} = #{item}"}.join("\n")}
+      end
+
+      def ==(insn)
+        super(insn) && #{args.collect{|item| "@#{item} == insn.#{item}"}.join('&&')}
+      end
+    end
+    INSTRUCDTION
+  end
 end
